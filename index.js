ba12adfbbbbd9aa9e6c13251de00d3127e6df05d
Mahmood Nihad <m.zibari@gmail.com>
2020-03-15 16:41:10 -0500

    remove stopVideo

--- index.js
+++ index.js
@@ -38,7 +38,6 @@ function determineVideoSize() {
 function displayResults(responseJson) {
     console.log(responseJson);
     vidId = responseJson.items[0].id.videoId.toString();
-    alert(vidId);
     loadVideo(vidId);
     $('#player').removeClass('hidden');
 };
@@ -97,7 +96,7 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// 3. This function creates an <iframe> (and YouTube player)
+// This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
 var player;
 function onYouTubeIframeAPIReady() {
@@ -106,31 +105,19 @@ function onYouTubeIframeAPIReady() {
         // Height divided by 2 to equate to 2:1 aspect ratio
         height: `${dims.width / 2}`,
         width: `${dims.width}`,
-        videoId: 'LQUXuQ6Zd9w',
+        videoId: '',
         events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
+            'onReady': onPlayerReady
         }
     });
 }
 
-// 4. The API will call this function when the video player is ready.
+// The API will call this function when the video player is ready.
 function onPlayerReady(event) {
     event.target.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
-//    The function indicates that when playing a video (state=1),
-//    the player should play for six seconds and then stop.
-/*var done = false;
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING && !done) {
-        setTimeout(stopVideo, 6000);
-        done = true;
-    }
-}
-function stopVideo() {
-    player.stopVideo();
-}*/
+
+
 
 $(watchForm);
