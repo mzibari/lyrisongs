0a820378388d5d7f10317f4f706f21c9320dcd11
Mahmood Nihad <m.zibari@gmail.com>
2020-03-24 19:55:06 -0500

    adjust video request

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ function watchForm() {
     });
     $('#js-form-song').submit(event => {
         event.preventDefault();
-        const searchSong = $('#js-search-song').val();
+        const searchSong = $('#js-search-song').val() + ' ' + $('#js-search-artist').val();
         getLyrics();
         getYouTubeVideoId(searchSong, 1);
     });
